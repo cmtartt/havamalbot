9cfe393f7b3fc983134c6b9f5f02bd064ddc5c10
Chloe Tartt <empress@everheartempire.com>
2026-01-19 11:28:03 -0800

    Moderate updates.

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const bellowsJson = require('./assets/bellows.json');
 const brayJson = require('./assets/bray.json');
 const pettitJson = require('./assets/petit.json');
 const icelandicJson = require('./assets/icelandic.json');
+const thorpeJson = require('./assets/thorpe.json');
+const hollanderJson = require('./assets/hollander.json');
 
-
-// 
+//
 
 console.log(process.env);
 
@@ -30,42 +31,61 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 client.on('messageCreate', async message => {
 
+	function magic(arg) {
+	    switch(arg) {
+		case 1:
+		message.channel.send("https://tenor.com/view/shrimp-as-that-clash-royale-hee-hee-hee-haw-gif-25054781");
+		return;
+		break;
+	    }
+	}
+
         // Ignore messages from other bots to prevent infinite loops
         if (message.author.bot) return;
-        
-        if (message.content.toLowerCase().indexOf("!havamal") == 0) { 
+
+        if (message.content.toLowerCase().indexOf("!havamal") == 0) {
             console.log("Begin havamal parsing");
             const messageSplits = message.content.toLowerCase().split(' ');
             console.log(messageSplits);
-            // assume first split is command, 2nd is 
-            var stanzaId = 0;
-            
-            if(messageSplits[1]) { 
-                try {                     
-                    stanzaId = parseInt(messageSplits[1], 10) - 1;                    
-                } catch(e) { 
+            // assume first split is command, 2nd is
+            var stanzaId = Math.floor(Math.random() * 164);
+   	    var translation = 'pettit';
+	    var hasStanzaOverride = false;
+	    var hasTranslationOverride = false;
+
+	    for(split in messageSplits) {
+	    	console.log(split);
+		try {
+                    const parsedInt = parseInt(messageSplits[split], 10) - 1;
+		    if(isNaN(parsedInt)) {
+			translation = messageSplits[split]
+		    	hasTranslationOverride = true;
+		    } else {
+			stanzaId = parsedInt;
+		    	hasStanzaOverride = true;
+		    }
+                } catch(e) {
                     console.log(e);
                     message.channel.send("Invalid stanza");
                     return;
                 }
-                
-                if(stanzaId > 164 || stanzaId < 0 || isNaN(stanzaId)) { 
+	    }
+
+            if(hasStanzaOverride) {
+                if(stanzaId > 164 || stanzaId < 0) {
                     message.channel.send("Invalid stanza");
                     return;
-                }   
+                }
 
                 console.log("DEBUG: " + stanzaId);
-            } else { 
-                stanzaId = Math.floor(Math.random() * 164);
             }
-            
+
             var stanzaText = '';
             var attestationText = '';
-            
-            if(messageSplits[2]) { 
-                const translation = messageSplits[2];
-                switch(translation) { 
-                    case 'bray': 
+
+            if(hasTranslationOverride) {
+                switch(translation) {
+                    case 'bray':
                         stanzaText = brayJson.data[stanzaId].text;
                         attestationText = "Bray's Translation";
                         break;
@@ -73,28 +93,34 @@ client.on('messageCreate', async message => {
                         stanzaText = bellowsJson.data[stanzaId].text;
                         attestationText = "Bellows's Translation";
                         break;
-                    case 'icelandic': 
+                    case 'icelandic':
                         stanzaText = icelandicJson.data[stanzaId].text;
                         attestationText = "Icelandic Text";
                         break;
+		    case 'thorpe':
+			stanzaText = thorpeJson.data[stanzaId].text;
+			attestationText = "Thorpe's Translation";
+			break;
+		    case 'hollander':
+			stanzaText = hollanderJson.data[stanzaId].text;
+			attestationText = "Hollander's Translation";
+			break;
+		    case 'woolsey':
+			magic(1);
+			return;
+			break;
                     case 'pettit':
                     default:
                         stanzaText = pettitJson.data[stanzaId].text;
                         attestationText = "Pettit's Translation";
                         break;
                 }
-            } else { 
-                stanzaText = pettitJson.data[stanzaId].text;
-                attestationText = "Pettit's Translation";
             }
 
-            stanzaText += "- Havamal, stanza " + (stanzaId + 1) + ` (${attestationText})`;
+            stanzaText += "-- Havamal, stanza " + (stanzaId + 1) + ` (${attestationText})`;
 
             console.log("Sending stanzaId: " + stanzaId);
             message.channel.send(stanzaText);
-            if(stanzaId == 68) { 
-                message.channel.send("nice.");
-            }
         }
     });
 
